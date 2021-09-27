43b200b9c25ee47468b3857ebd77eb08f20f3211
Gözde Altuntaş <altuntasgozde@gmail.com>
2021-09-27 14:27:16 +0300

    api adress update

--- app.js
+++ app.js
@@ -8,21 +8,22 @@ button.addEventListener("click", change);
 
 function change(){
     const xhr = new XMLHttpRequest();
-    xhr.open("GET","https://api.exchangeratesapi.io/latest?base=TRY");
+    xhr.open("GET","https://v6.exchangerate-api.com/v6/d82e33377c0590c246b3675a/latest/TRY");
 
     xhr.onload = function() {
         if(this.status === 200){
             const response = JSON.parse(this.responseText);
+            console.log(response)
             // USD
-            const usdRate = response.rates.USD;
+            const usdRate = response.conversion_rates.USD;
             const amountUsd = Number(entryInput.value);
             usdInput.value = usdRate*amountUsd;
             // EUR
-            const eurRate = response.rates.EUR;
+            const eurRate = response.conversion_rates.EUR;
             const amountEur = Number(entryInput.value);
             eurInput.value = eurRate*amountEur;
             // GBP 
-            const gbpRate = response.rates.GBP;
+            const gbpRate = response.conversion_rates.GBP;
             const amountGbp = Number(entryInput.value);
             gbpInput.value = gbpRate*amountGbp;
         }
