f20e059a56e6e52265540230c8b8ee945ec9a02d
Gözde <altuntasgozde@gmail.com>
2022-04-28 17:17:38 +0300

    API updated

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ button.addEventListener("click", change);
 
 function change(){
     const xhr = new XMLHttpRequest();
-    xhr.open("GET","https://v6.exchangerate-api.com/v6/d82e33377c0590c246b3675a/latest/TRY");
+    xhr.open("GET","https://api.currencyapi.com/v3/latest?apikey=6eb298f0-5465-11ec-89a1-f78998a370de&currencies=EUR%2CUSD%2CGBP&base_currency=TRY");
 
     xhr.onload = function() {
         if(this.status === 200){
             const response = JSON.parse(this.responseText);
             console.log(response)
             // USD
-            const usdRate = response.conversion_rates.USD;
+            const usdRate = response.data.USD.value;
             const amountUsd = Number(entryInput.value);
             usdInput.value = usdRate*amountUsd;
             // EUR
-            const eurRate = response.conversion_rates.EUR;
+            const eurRate = response.data.EUR.value;
             const amountEur = Number(entryInput.value);
             eurInput.value = eurRate*amountEur;
             // GBP 
-            const gbpRate = response.conversion_rates.GBP;
+           const gbpRate = response.data.GBP.value;
             const amountGbp = Number(entryInput.value);
             gbpInput.value = gbpRate*amountGbp;
         }
